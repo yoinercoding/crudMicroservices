023bacd5a89f3666954cf476435ae83332d43370
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-07-04 14:08:02 -0300

    update, separado el proyecto por microservicios

---
+++ gatewayService/src/main/java/com/crud/gateway/CrudGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.crud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrudGatewayApplication.class, args);
+    }
+}
---
+++ gatewayService/src/test/java/com/crud/gateway/AwesomeAppApiGatewayApplicationTest.java
@@ -0,0 +1,4 @@
+package com.crud.gateway;
+
+public class AwesomeAppApiGatewayApplicationTest {
+}
